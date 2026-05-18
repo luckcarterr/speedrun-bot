cfabcea46f60955c78b76574fce31e7effc01ceb
luckcarterr <146742749+luckcarterr@users.noreply.github.com>
2026-05-18 14:46:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,21 +33,21 @@ def parse_time(time_str: str) -> float:
 def heist_coefficient(median_seconds: float) -> float:
     """Return the HeistCoefficient based on the median run time."""
     if median_seconds < 15:
-        return 3.0
+        return 2.35
     elif median_seconds < 30:
-        return 2.5
+        return 2.15
     elif median_seconds < 60:
-        return 2.25
-    elif median_seconds < 120:
         return 2.0
-    elif median_seconds < 240:
+    elif median_seconds < 120:
         return 1.75
-    elif median_seconds < 300:
+    elif median_seconds < 240:
         return 1.5
+    elif median_seconds < 300:
+        return 1.25
     elif median_seconds < 600:
         return 1.0
     else:
-        return 0.75
+        return 0.85
  
  
 def format_seconds(seconds: float) -> str:
